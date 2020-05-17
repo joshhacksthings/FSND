5fa26c6ffe90773a4459cc2a9ce09e2e6937b934
joshhacksthings <jdnowak01@gmail.com>
2020-05-17 12:57:46 -0700

    export postman collection, finish frontend

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-68yxhfrv.auth0.com', // the auth0 domain prefix
+    url: 'dev-68yxhfrv', // the auth0 domain prefix
     audience: 'coffee_shop_full_stack', // the audience set for the auth0 app
     clientId: 'pZjWJLd7oCeeyfESLBTkd3iWlr30eA3u', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
