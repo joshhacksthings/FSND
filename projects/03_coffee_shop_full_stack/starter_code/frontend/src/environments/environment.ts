553f3254176295b6ead5e90b1036b7acfb4a1f2a
joshhacksthings <jdnowak01@gmail.com>
2020-05-17 12:13:05 -0700

    update readme, populate environment.ts

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-68yxhfrv.auth0.com', // the auth0 domain prefix
+    audience: 'coffee_shop_full_stack', // the audience set for the auth0 app
+    clientId: 'pZjWJLd7oCeeyfESLBTkd3iWlr30eA3u', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
